a214d88a9275c5dfabb4a32459639b5b74f75bd8
alanoud-GA <alanoud.alrasheed@generalassemb.ly>
2019-08-20 10:52:34 +0300

    Updates

---
+++ precache-manifest.a16cc50daf3bb0969d3c54e7d3e8e159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4114d5b489578803ae65980a5451a564",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "6cd613c4d29138424c04",
+    "url": "/test/static/css/main.d67ef44b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/test/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6cd613c4d29138424c04",
+    "url": "/test/static/js/main.b651946e.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
